895a10a0bb8515e47810cfcc5f0e7ae79c7ef083
Mingxiang.Zhang <ming.zhang@gree.net>
2013-05-22 10:41:29 +0900

    add directory_root parameter

--- manifests/register.pp
+++ manifests/register.pp
@@ -1,6 +1,6 @@
- define lsyncd::register ($directories, $order=10) {
+ define lsyncd::register ($directories, $directory_root, $order=10) {
    concat::fragment{"lsyncd_${title}":
-      target  => lsyncd::conf_file,
+      target  => $lsyncd::conf_file,
       content => template('lsyncd/lsyncd_csync2_localsources.erb'),
    }
  } 
